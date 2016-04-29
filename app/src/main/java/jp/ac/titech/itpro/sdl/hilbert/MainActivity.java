867ab533c53a4e0f95bc7c80fa7a169c4a907cbc
hmoriz <dummy@example.com>
2016-04-29 15:52:54 +0900

    Add Log.d and get rotatable

--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
@@ -1,7 +1,8 @@
 package jp.ac.titech.itpro.sdl.hilbert;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,19 +11,62 @@ public class MainActivity extends AppCompatActivity {
     private HilbertView hilbertView;
     private final static int MAX_N = 9;
     private int n = 1;
+    private static final String TAG = "MainActivity";
+    private static final String KEY_N = "MainActivity.n";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(savedInstanceState != null){
+            n = savedInstanceState.getInt(KEY_N, 1);
+        }
         setContentView(R.layout.activity_main);
         levelView = (TextView)findViewById(R.id.level_view);
         hilbertView = (HilbertView)findViewById(R.id.hilbert_view);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         display();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_N, n);
     }
 
     public void onClickDec(View v) {
